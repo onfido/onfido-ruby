8c32ecc579aa40733cf8af5f552c200ac718cf3c
Daniel Olivares <danieloddl2@hotmail.com>
2023-01-16 14:53:12 -0600

    feat: Add Monitor#list_matches, #set_match_status

--- lib/onfido/resources/monitor.rb
+++ lib/onfido/resources/monitor.rb
@@ -20,5 +20,13 @@ module Onfido
     def destroy(monitor_id)
       delete(path: "watchlist_monitors/#{monitor_id}")
     end
+
+    def list_matches(monitor_id)
+      get(path: "watchlist_monitors/#{monitor_id}/matches")
+    end
+
+    def set_match_status(monitor_id, **payload)
+      patch(path: "watchlist_monitors/#{monitor_id}/matches", payload: payload)
+    end
   end
 end
--- spec/integrations/monitor_spec.rb
+++ spec/integrations/monitor_spec.rb
@@ -34,7 +34,67 @@ describe Onfido::Monitor do
     end
   end
 
-  it 'returns success code' do
-    expect { monitor.destroy(monitor_id) }.not_to raise_error
+  describe '#destroy' do
+    it 'returns success code' do
+      expect { monitor.destroy(monitor_id) }.not_to raise_error
+    end
+  end
+
+  describe '#list_matches' do
+    subject(:list_matches) { monitor.list_matches(monitor_id) }
+
+    it do
+      is_expected
+        .to include('matches' => [hash_including('id' => anything, 'enabled' => true), anything])
+    end
+
+    context 'when the monitor_id does not exist' do
+      let(:monitor_id) { '00000000-c6b8-4c1e-a383-21efa241ce1e' }
+
+      it 'raises an error' do
+        expect { list_matches }.to raise_error(Onfido::RequestError, /404/)
+      end
+    end
+  end
+
+  describe '#set_match_status' do
+    subject(:set_match_status) { monitor.set_match_status(monitor_id, **payload) }
+
+    let(:payload) do
+      {
+        disabled: ['925e47d3-9e21-48d4-b570-9564c4282cec'],
+        enabled: ['37afcacb-5ff0-4b09-831d-5bcc672ca1fb']
+      }
+    end
+
+    it do
+      is_expected
+        .to include('matches' => [hash_including('id' => anything, 'enabled' => true), anything])
+    end
+
+    context 'when a match id is both in enabled and disabled' do
+      before { payload[:disabled] << payload[:enabled][0] }
+
+      it 'raises an error' do
+        expect { set_match_status }
+          .to raise_error(
+            an_instance_of(Onfido::RequestError).and(having_attributes(response_code: 422))
+          )
+      end
+    end
+
+    context 'when not passing any enabled or disabled ids' do
+      let(:payload) { {} }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when the monitor_id does not exist' do
+      let(:monitor_id) { '00000000-c6b8-4c1e-a383-21efa241ce1e' }
+
+      it 'raises an error' do
+        expect { set_match_status }.to raise_error(Onfido::RequestError, /404/)
+      end
+    end
   end
 end
