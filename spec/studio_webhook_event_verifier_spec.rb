aaea2378e04add3556dbf5e9f4d04021d318c498
Davide Vacca <davide.vacca@onfido.com>
2024-12-20 11:55:11 +0100

    Update tests after library refresh

--- spec/studio_webhook_event_verifier_spec.rb
+++ spec/studio_webhook_event_verifier_spec.rb
@@ -35,5 +35,44 @@ describe Onfido::WebhookEventVerifier do
       expect(webhook_event.payload.resource.task_def_id).to eq('profile_data')
       expect(webhook_event.payload.resource.output).to eq(nil)
     end
+
+    context "when webhook event output is an object" do
+      it "is able to decode and read the output field" do
+        signature = "e3e5565647f5ccf07b2fd8ac22eab94a0a0619413d981fb768295c820523f7d7"
+
+        event = subject.read_payload(File.read('spec/media/studio_webhook_event_with_object_in_output.json'), signature)
+
+        expect(event.payload.resource.output['properties']).to eq({
+          "date_of_birth" => "1990-01-01",
+          "date_of_expiry" => "2031-05-28",
+          "document_number" => "999999999",
+          "document_numbers" => [
+            {
+              "type"=>"document_number",
+              "value"=>"999999999"
+            }
+          ],
+          "document_type" => "passport",
+          "first_name" => "Jane",
+          "issuing_country" => "GBR",
+          "last_name" => "Doe",
+        })
+      end
+    end
+
+    context "when webhook event output is an array" do
+      it "is able to decode and read the output field" do
+        signature = "f3a5170acfcecf8c1bf6d9cb9995c0d9dec941af83056a721530f8de7af2c293"
+        event = subject.read_payload(File.read('spec/media/studio_webhook_event_with_list_in_output.json'), signature)
+
+        expect(event.payload.resource.output).to eq([
+          {
+            "checksum_sha256"=>"hiwV2PLmeQZzeySPGGwVL48sxVXcyfpXy9LDl1u3lWU=",
+            "id"=>"7af75a3a-ba34-4aa5-9e3e-096c9f56256b",
+            "type"=>"document_photo"
+          }
+        ])
+      end
+    end
   end
 end
